6eacd04570a91e213710cd0114cfa1be11f58393
Weiming Zhai <wmzhai@gmail.com>
2017-02-18 00:17:20 +0800

    use expressGraphQL to handle request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+//任何时候访问如下路径时，让expressGraphQL处理
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
 
 app.listen( 4000, () => {
   console.log('Listening');
