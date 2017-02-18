7476afc3c24bddd1fe5c92691ab0e5ac7de46b87
Weiming Zhai <wmzhai@gmail.com>
2017-02-18 20:14:27 +0800

    nest queries

--- schema/schema.js
+++ schema/schema.js
@@ -8,12 +8,29 @@ const {
   GraphQLSchema
 } = graphql;
 
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then( res => res.data );
+      }
+    }
   }
 });
 
@@ -25,7 +42,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
-          .then( resp => resp.data);
+          .then( res => res.data);
       }
     }
   }
