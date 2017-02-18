03ab1e3ab0072e1f58f7a4f3e930398783f4f059
Weiming Zhai <wmzhai@gmail.com>
2017-02-18 14:54:27 +0800

    add schema and rootquery

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,39 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Weiming Zhai', age : 30 },
+  { id: '47', firstName: 'Tom', age: 21}
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
-    firstName: { type: GraphQLString},
-    age:  { type: GraphQLInt},
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 //任何时候访问如下路径时，让expressGraphQL处理
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }))
 
-app.listen( 4000, () => {
+app.listen( 3000, () => {
   console.log('Listening');
 });
